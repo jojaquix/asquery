46d267c996d82d934be7a7ecdbd5194c015a086a
Jhon James Quintero <jhon.quintero@alertlogic.com>
2018-09-28 11:47:11 -0500

    Better messages in build script

--- cmd/asquery/build.ps1
+++ cmd/asquery/build.ps1
@@ -1,5 +1,10 @@
 $TARGETS_CONFS = ("windows","386"),("windows","amd64") 
 
+Write-Host "[*] Starting asquery build" -ForegroundColor DarkGreen
+
+# Time our execution
+$sw = [System.Diagnostics.StopWatch]::startnew()
+
 foreach ($i in $TARGETS_CONFS) {
     Write-Host "Building asquery for: " $i 
     $env:GOOS = $i[0]
@@ -10,15 +15,15 @@ foreach ($i in $TARGETS_CONFS) {
         if ($i[0] -eq "windows") {
             $output_file_name = $output_file_name+".exe"
         }
-
         go build -o $output_file_name
-
-        Write-Host "Finished building" -ForegroundColor green 
+        Write-Host "Finished building" -ForegroundColor Green
     }
     catch {
-        Write-Host "Something failed" -ForegroundColor red 
+        Write-Host "Something failed" -ForegroundColor Red 
     }
 }
+
+Write-Host "[*] Build took $($sw.ElapsedMilliseconds) ms" -ForegroundColor DarkGreen
 
 
 
